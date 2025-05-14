11fc1f4eb56f4ca7a33cd87cbccb53db52ac9414
Elisabeth Giryes <elisabeth.giryes@uzh.ch>
2025-05-14 15:49:53 +0200

    setup + sub-sampling

---
+++ scripts/sub_sampling.sh
@@ -0,0 +1,17 @@
+# Set paths
+base=$(dirname "$0")/..
+data=$base/data
+sampled_data=$base/sampled_data
+
+mkdir -p $sampled_data
+
+# Sub-sample to 100k lines
+head -n 100000 $data/train.de-it.de > $sampled_data/train.de-it.de
+head -n 100000 $data/train.de-it.it > $sampled_data/train.de-it.it
+
+cp $data/dev.de-it.de $sampled_data/dev.de-it.de
+cp $data/dev.de-it.it $sampled_data/dev.de-it.it
+cp $data/test.de-it.de $sampled_data/test.de-it.de
+cp $data/test.de-it.it $sampled_data/test.de-it.it
+
+echo "Sub-sampling complete: 100k de-it sentence pairs in $sampled_data"
