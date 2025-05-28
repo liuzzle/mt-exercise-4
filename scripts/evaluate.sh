5cf93a47b6f984443b585bcd8969d86f8cc3a8ff
Elisabeth Giryes <elisabeth.giryes@uzh.ch>
2025-05-28 17:38:04 +0200

    modify evaluation script + translate with 10 different beam sizes

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -10,8 +10,8 @@ translations=$base/translations
 
 mkdir -p $translations
 
-src=?
-trg=?
+src=de-it.de
+trg=de-it.it
 
 
 num_threads=4
@@ -21,7 +21,7 @@ device=0
 
 SECONDS=0
 
-model_name=?
+model_name="transformer_sample_config"
 
 echo "###############################################################################"
 echo "model_name $model_name"
@@ -30,11 +30,11 @@ translations_sub=$translations/$model_name
 
 mkdir -p $translations_sub
 
-CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name.yaml < $data/test.$src > $translations_sub/test.$model_name.$trg
+CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name.yaml < $data/test.$src > $translations_sub/test_beam10.$model_name.$trg
 
-# compute case-sensitive BLEU 
+# compute case-sensitive BLEU
 
-cat $translations_sub/test.$model_name.$trg | sacrebleu $data/test.$trg
+cat $translations_sub/test_beam10.$model_name.$trg | sacrebleu $data/test.$trg
 
 
 echo "time taken:"
